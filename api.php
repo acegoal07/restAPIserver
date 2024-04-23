bb1122e34cef501c69af6f5200d65ef146b90e68
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-04-23 16:48:00 +0100

    Initial push

---
+++ api.php
@@ -0,0 +1,92 @@
+<?php
+
+class API
+{
+   private $conn;
+
+   public function __construct()
+   {
+      $servername = "localhost";
+      $port = "3306";
+      $username = "root";
+      $password = "";
+      $dbname = "apidatabase";
+      $this->conn = new mysqli($servername, $username, $password, $dbname, $port);
+   }
+
+   public function handleRequest()
+   {
+      if ($this->conn->connect_error) {
+         http_response_code(500);
+      } else {
+         switch ($_SERVER['REQUEST_METHOD']) {
+            case 'POST':
+               if (!isset($_POST['oid']) ||
+               strlen($_POST['oid']) > 32 ||
+               !isset($_POST['name']) ||
+               strlen($_POST['name']) > 64 ||
+               !isset($_POST['comment']) ||
+               strlen($_POST['comment']) > 255) {
+                  http_response_code(400);
+               } else {
+                  $this->handlePost();
+               }
+               break;
+            case 'GET':
+               if (!isset($_GET['oid']) || strlen($_GET['oid']) > 32) {
+                  http_response_code(400);
+               } else {
+                  $this->handleGet();
+               }
+               break;
+            default:
+               http_response_code(400);
+               break;
+         }
+      }
+   }
+
+   public function handlePost()
+   {
+      try {
+         $oid = $_POST['oid'];
+         $name = $_POST['name'];
+         $comment = $_POST['comment'];
+         $id = null;
+         if ($this->conn->query("INSERT INTO `comments`(`oid`, `name`, `comment`) VALUES ('$oid', '$name','$comment')")) {
+            $id = $this->conn->insert_id;
+         }
+         $response["id"] = $id;
+         if (http_response_code(201)) {
+            echo json_encode($response);
+         }
+      } catch (Exception $e) {
+         http_response_code(500);
+      }
+   }
+
+   public function handleGet()
+   {
+      try {
+         $oid = $_GET['oid'];
+         $result = $this->conn->query("SELECT `id`, `date`, `name`, `comment` FROM comments where `oid` = $oid");
+         $data = array();
+         if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+               array_push($data, $row);
+            }
+         }
+         $result->close();
+         $response["oid"] = $oid;
+         $response["comments"] = $data;
+         if (http_response_code(201)) {
+            echo json_encode($response);
+         }
+      } catch (Exception $e) {
+         http_response_code(500);
+      }
+   }
+}
+
+$api = new API();
+$api->handleRequest();
---
+++ apidatabase.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2024 at 04:56 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apidatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `oid` varchar(32) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `name` varchar(64) NOT NULL,
+  `comment` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
